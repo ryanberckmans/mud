217b302de303298d1bcc87f3a71b9df87082160f
Ryan <rberckma@rberckma.(none)>
2009-03-10 20:08:09 -0400

    1. test.py correctly dynamically loads unit tests

--- src/test.py
+++ src/test.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+
 def loadTestModules():
     import os
-    import unittests
+    import mud
 
     modules = []
 
-    for f in os.listdir(os.path.abspath(unittests.__path__[0])):
-        module, ext = os.path.splitext(f) # Handles no-extension files, etc.      
-        if ext == '.py': # Important, ignore .pyc/other files.
-            print 'imported module: %s' % (module)
-            modules.append(__import__(module))
+    for pkgName in mud.__all__:
+        __import__("mud.%s.unittests" % pkgName)
+        for f in os.listdir(os.path.abspath("mud/%s/unittests" % pkgName )):
+            module, ext = os.path.splitext(f) # Handles no-extension files, etc.      
+            if ext == '.py' and module != '__init__': # Important, ignore .pyc/other files.
+                print 'imported module: mud.%s.unittests.%s' % (pkgName, module)
+                modules.append(__import__("mud.%s.unittests.%s" % (pkgName,module) ) )
 
     return modules
 
@@ -21,16 +24,18 @@ if __name__ == '__main__':
     print "\nLOADING TESTS...\n"
 
     modules = loadTestModules()
+
+    tests = []
     
     for module in modules:
         for name in dir(module):
             obj = getattr(module, name)
             if inspect.isclass(obj):
-                all_tests.append( unittest.TestLoader().loadTestsFromTestCase(obj))
+                tests.append( unittest.TestLoader().loadTestsFromTestCase(obj))
                 print "loaded test: ", obj
 
     print "\nRUNNING TESTS...\n"
 
-    unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(all_tests))
+    unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(tests))
     
 
