67c769eb90d2aa43a74555fe9005017f26c3f046
Ryan Berckmans <rberckma@ubuntu-dev-vm.(none)>
2009-03-25 19:15:17 -0400

    1. welcome bug

--- src/mods-available/welcome/welcome.py
+++ src/mods-available/welcome/welcome.py
@@ -2,17 +2,7 @@ import mud.core.mod as mod
 import mud.core.signals as signals
 
 
-welcomeMessage = """{@{!{FG     ________             ________
-{FG    / ____  /\           /\  ____ \ 
-{FG   / /\__/ / _\_________/_ \ \__/\ \ 
-{FG  / /_/_/ / /             \ \ \_\_\ \ 
-{FG /_______/ /_______________\ \_______\ 
-{FG \  ____ \ \               / / ____  /
-{FG  \ \ \_\ \ \_____________/ / /_/ / /
-{FG   \ \/__\ \  /{FR N O T A{FG \  / /__\/ /
-{FG    \_______\/{FY M   U   D{FG \/_______/
-{FG 
-       {BR{FW+{BB {FRC a t a l y s t i c a {BR{FW+{@\r\n\r\n%s"""
+welcomeMessage = """{@{!{FG     ________             ________ \r\n{FG    / ____  /\           /\  ____ \ \r\n{FG   / /\__/ / _\_________/_ \ \__/\ \ \r\n{FG  / /_/_/ / /             \ \ \_\_\ \ \r\n{FG /_______/ /_______________\ \_______\ \r\n{FG \  ____ \ \               / / ____  / \r\n{FG  \ \ \_\ \ \_____________/ / /_/ / / \r\n{FG   \ \/__\ \  /{FR N O T A{FG \  / /__\/ / \r\n{FG    \_______\/{FY M   U   D{FG \/_______/ \r\n{FG  \r\n       {BR{FW+{BB {FRC a t a l y s t i c a {BR{FW+{@ \r\n \r\n%s"""
 
 
 def welcomeCallback( client ):
